935d4d77ad3036b64a47d7efa00296e722b9ff90
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-07-31 23:19:59 +0630

    Update f1.js

--- f1.js
+++ f1.js
@@ -9,7 +9,7 @@ function smotor() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/linea8.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function daznf1() {
- document.getElementById("objek").innerHTML = '<iframe src="intent://927764536234165419.ofds0401.xyz:80/play/fe715725/3pTCaErhC6/165419.ts|&User-Agent=Mozilla/5.0#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+ document.getElementById("objek").innerHTML = '<iframe src="intent://927764536234165419.ofds0401.xyz:80/play/fe715725/3pTCaErhC6/165419.ts#Intent;scheme=http;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function celestialmoviech1() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://cempedak-live-cdn.mncnow.id/live/eds/CelestialMovie/sa_dash_vmx/CelestialMovie.mpd?|referer=https://www.visionplus.id&drmScheme=clearkey&drmLicense=13aecb26aeee22bc86920045f22d134d:80aa44d998c7c0cc221c96d26730fe9b#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
