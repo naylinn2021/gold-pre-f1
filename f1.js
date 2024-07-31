036e4950307c9639b19a62b6b8d6eec6b1079ba0
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-07-31 17:23:06 +0630

    Update f1.js

--- f1.js
+++ f1.js
@@ -3,7 +3,7 @@ function skyf1() {
 };
 
 function sky2f1() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.online/uk-skysportf1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.online/uk-skysportf1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function smotor() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://cdn4.aluku91.foundation/linea8.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
