cb942b5246b14763029beb48fbe1fcfa7979b55c
mmgold <77553706+naylinn2021@users.noreply.github.com>
2024-08-24 00:23:41 +0630

    Update f1.js

--- f1.js
+++ f1.js
@@ -1,7 +1,5 @@
-
-
 function skyf1() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.online/uk-skysportf1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn.aluku91.foundation/uk-skysportf1.m3u8|referer=https://365streamtv.live/origin=https://365streamtv.live/&User-Agent=Mozilla/5.0#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=G⚽LD PREMIUM;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 
 function sky2f1() {
